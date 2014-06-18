2cd666d2a7040e3a855f700ea8e72acaa0cd1ed2
gladson <gladsonvm@yahoo.com>
2014-06-18 19:56:09 +0530

    added models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from random import randrange
+from datetime import datetime, timedelta
+from hashlib import md5
 
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import utc
+from django.utils import timezone
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.Foreignkey(User)
+    user_image = models.TextField(null=True, blank=True)
+    activation_key = models.IntegerField(null=True, blank=True)
+    key_expires = models.DateTimeField(null=True, blank=True)
+    dept = models.TextField(null=True, blank=True)
+
+
+class ClientProfile(models.Model):
+    client = models.ForeignKey(User)
+    dept = models.TextField(null=True,blank=True)
+    question = models.TextField(max_length=None)
+
+class ClientChat(models.Model):
+    client = models.ForeignKey(ClientProfile)
+    asst = models.ForeignKey(UserProfile)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField(null=True,blank=True)
+    content = models.TextField(max_length=None)
+
+    def duration(self):
+        duratn = (ended_at-started_at)
+
+class OperatorChat(models.Model):
+    frm_op = models.ForeignKey(UserProfile)
+    to_op = models.ForeignKey(UserProfile)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField(null=True,blank=True)
+    content = models.TextField(max_length=None)
+
+    def duration(self):
+        duratn = (ended_at-started_at)
+
